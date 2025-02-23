b54d007dd5aa7eddf4b1f306b80de4366ac7106c
mashukak <mashenkast12@gmail.com>
2025-02-23 21:16:16 +0100

    attempt 1

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -53,7 +53,7 @@ export const selectContacts = (state) => state.contacts.items;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
-// Мемоізований селектор для фільтрації контактів
+
 export const selectFilteredContacts = createSelector(
   [selectContacts, (state) => state.filters.name],
   (contacts, filter) =>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // Use localStorage or sessionStorage
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-// Create persist configuration
 const persistConfig = {
-  key: "root", // key for the persisted state
-  storage, // Choose storage method (localStorage by default)
+  key: "root",
+  storage, 
 };
 
-// Combine your reducers
+
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer,
 });
 
-// Wrap the combined reducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  // Disable serializability checks for redux-persist actions
+  
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'], // Ignore persist-related actions
-        ignoredPaths: ['register'], // Optionally, ignore specific paths
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+        ignoredPaths: ['register'], 
       },
     }),
 });
 
-export const persistor = persistStore(store); // Create persistor
+export const persistor = persistStore(store); 
