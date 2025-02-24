eeef7c52361b6a7ce8b45fb7d3d4a1ac134c72cf
mashukak <mashenkast12@gmail.com>
2025-02-24 14:03:41 +0100

    attempt 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { combineReducers } from "redux";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-const persistConfig = {
-  key: "root",
-  storage, 
-};
-
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filtersReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-        ignoredPaths: ['register'], 
-      },
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
 });
-
-export const persistor = persistStore(store); 
